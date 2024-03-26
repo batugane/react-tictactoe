477749bf421615d8135a3d00e89c292658f97551
batuhanDuras <batugane@gmail.com>
2024-03-26 09:30:13 +0300

    Revert "feat: replay option"
    This reverts commit c39b0d0450dcef3ad0851011ccc509f027edf0ba.

--- src/utils/calculateWinner.ts
+++ src/utils/calculateWinner.ts
@@ -8,14 +8,14 @@ export function calculateWinner(squares: string[]): string | null {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ]
+  ];
 
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return squares[a];
     }
   }
 
-  return null
+  return null;
 }
